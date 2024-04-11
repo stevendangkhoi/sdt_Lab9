27c262da576283a441e79a95c6edf4a90215e34f
tylertuck <166671176+tylertuck@users.noreply.github.com>
2024-04-10 23:13:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def encode(num):
     return num+3
 
@@ -7,17 +8,20 @@ def decode(num):
 def main():
     storeNum = None
     encodedNum = ''
+    decodedNum = ''
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
         choice = int(input("Please enter an option: "))
         if choice == 1:
             pw = input("Please enter your password to encode: ")
-            storeNum = int(pw)
             for n in pw:
                 encodedNum += str(encode(int(n)))
+                storeNum = encodedNum
             print("Your password has been encoded and stored!\n")
         elif choice == 2:
-            print("The encoded password is " + encodedNum + ", and the original password is " + pw + ".\n")
+            for n in storeNum:
+                decodedNum += str(decode(int(n)))
+            print("The encoded password is " + encodedNum + ", and the original password is " + decodedNum + ".\n")
         elif choice == 3:
             break
 
